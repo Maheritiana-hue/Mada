9412cb56462209f44a01a805f18f341dce6f1e87
Maheritina <andriarimanana213@gmail.com>
2025-06-27 09:55:45 +0200

    v-1

--- quiz.js
+++ quiz.js
@@ -1,16 +1,16 @@
 function calculerScore(){
   let score = 0;
-  for(let i = 1; i <= 40; i++){
+  for(let i = 1; i <= 30; i++){
     const question = document.querySelector('input[name="q' + i +'"]:checked');
     if(question){
       score += parseInt(question.value);
     }
   }
-  document.getElementById("resultat").textContent = "SCORE OBTENU  =  " + score + "/ 40";
+  document.getElementById("resultat").textContent = "SCORE OBTENU  =  " + score + "/ 30";
 }
 
 function recommencerQuiz(){
-  for(let i = 1; i <= 40; i++){
+  for(let i = 1; i <= 30; i++){
     const options = document.getElementsByName("q" + i);
     for(let j = 0; j < options.length; j++){
       options[j].checked = false;
